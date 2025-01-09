fb8493c89413f060d4427ecaeba1964ed1daa4b0
hanbyulkang <kanghanstar@gmail.com>
2025-01-09 16:32:50 +0900

    UUID 생성 기능 추가 및 코드 정리
    실험 이름에 UUID 생성을 추가하여 고유성을 보장했습니다. 또한 불필요한 공백과 들여쓰기를 정리하여 코드 가독성을 개선했습니다.

--- infer-web.py
+++ infer-web.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import uuid
+
 from dotenv import load_dotenv
 
 now_dir = os.getcwd()
@@ -32,7 +34,6 @@ import threading
 import shutil
 import logging
 
-
 logging.getLogger("numba").setLevel(logging.WARNING)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
@@ -49,17 +50,15 @@ os.environ["TEMP"] = tmp
 warnings.filterwarnings("ignore")
 torch.manual_seed(114514)
 
-
 config = Config()
 vc = VC(config)
 
-
 if config.dml == True:
-
     def forward_dml(ctx, x, scale):
         ctx.scale = scale
         res = x.clone().detach()
         return res
+
 
     fairseq.modules.grad_multiply.GradMultiply.forward = forward_dml
 i18n = I18nAuto()
@@ -74,30 +73,30 @@ if torch.cuda.is_available() or ngpu != 0:
     for i in range(ngpu):
         gpu_name = torch.cuda.get_device_name(i)
         if any(
-            value in gpu_name.upper()
-            for value in [
-                "10",
-                "16",
-                "20",
-                "30",
-                "40",
-                "A2",
-                "A3",
-                "A4",
-                "P4",
-                "A50",
-                "500",
-                "A60",
-                "70",
-                "80",
-                "90",
-                "M4",
-                "T4",
-                "TITAN",
-                "4060",
-                "L",
-                "6000",
-            ]
+                value in gpu_name.upper()
+                for value in [
+                    "10",
+                    "16",
+                    "20",
+                    "30",
+                    "40",
+                    "A2",
+                    "A3",
+                    "A4",
+                    "P4",
+                    "A50",
+                    "500",
+                    "A60",
+                    "70",
+                    "80",
+                    "90",
+                    "M4",
+                    "T4",
+                    "TITAN",
+                    "4060",
+                    "L",
+                    "6000",
+                ]
         ):
             # A10#A100#V100#A40#P40#M40#K80#A4500
             if_gpu_ok = True  # 至少有一张能用的N卡
@@ -263,14 +262,14 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version19, gpus_rmvp
     if if_f0:
         if f0method != "rmvpe_gpu":
             cmd = (
-                '"%s" infer/modules/train/extract/extract_f0_print.py "%s/logs/%s" %s %s'
-                % (
-                    config.python_cmd,
-                    now_dir,
-                    exp_dir,
-                    n_p,
-                    f0method,
-                )
+                    '"%s" infer/modules/train/extract/extract_f0_print.py "%s/logs/%s" %s %s'
+                    % (
+                        config.python_cmd,
+                        now_dir,
+                        exp_dir,
+                        n_p,
+                        f0method,
+                    )
             )
             logger.info("Execute: " + cmd)
             p = Popen(
@@ -292,16 +291,16 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version19, gpus_rmvp
                 ps = []
                 for idx, n_g in enumerate(gpus_rmvpe):
                     cmd = (
-                        '"%s" infer/modules/train/extract/extract_f0_rmvpe.py %s %s %s "%s/logs/%s" %s '
-                        % (
-                            config.python_cmd,
-                            leng,
-                            idx,
-                            n_g,
-                            now_dir,
-                            exp_dir,
-                            config.is_half,
-                        )
+                            '"%s" infer/modules/train/extract/extract_f0_rmvpe.py %s %s %s "%s/logs/%s" %s '
+                            % (
+                                config.python_cmd,
+                                leng,
+                                idx,
+                                n_g,
+                                now_dir,
+                                exp_dir,
+                                config.is_half,
+                            )
                     )
                     logger.info("Execute: " + cmd)
                     p = Popen(
@@ -319,12 +318,12 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version19, gpus_rmvp
                 ).start()
             else:
                 cmd = (
-                    config.python_cmd
-                    + ' infer/modules/train/extract/extract_f0_rmvpe_dml.py "%s/logs/%s" '
-                    % (
-                        now_dir,
-                        exp_dir,
-                    )
+                        config.python_cmd
+                        + ' infer/modules/train/extract/extract_f0_rmvpe_dml.py "%s/logs/%s" '
+                        % (
+                            now_dir,
+                            exp_dir,
+                        )
                 )
                 logger.info("Execute: " + cmd)
                 p = Popen(
@@ -334,7 +333,7 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version19, gpus_rmvp
                 done = [True]
         while 1:
             with open(
-                "%s/logs/%s/extract_f0_feature.log" % (now_dir, exp_dir), "r"
+                    "%s/logs/%s/extract_f0_feature.log" % (now_dir, exp_dir), "r"
             ) as f:
                 yield (f.read())
             sleep(1)
@@ -356,18 +355,18 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version19, gpus_rmvp
     ps = []
     for idx, n_g in enumerate(gpus):
         cmd = (
-            '"%s" infer/modules/train/extract_feature_print.py %s %s %s %s "%s/logs/%s" %s %s'
-            % (
-                config.python_cmd,
-                config.device,
-                leng,
-                idx,
-                n_g,
-                now_dir,
-                exp_dir,
-                version19,
-                config.is_half,
-            )
+                '"%s" infer/modules/train/extract_feature_print.py %s %s %s %s "%s/logs/%s" %s %s'
+                % (
+                    config.python_cmd,
+                    config.device,
+                    leng,
+                    idx,
+                    n_g,
+                    now_dir,
+                    exp_dir,
+                    version19,
+                    config.is_half,
+                )
         )
         logger.info("Execute: " + cmd)
         p = Popen(
@@ -463,20 +462,20 @@ def change_f0(if_f0_3, sr2, version19):  # f0method8,pretrained_G14,pretrained_D
 
 # but3.click(click_train,[exp_dir1,sr2,if_f0_3,save_epoch10,total_epoch11,batch_size12,if_save_latest13,pretrained_G14,pretrained_D15,gpus16])
 def click_train(
-    exp_dir1,
-    sr2,
-    if_f0_3,
-    spk_id5,
-    save_epoch10,
-    total_epoch11,
-    batch_size12,
-    if_save_latest13,
-    pretrained_G14,
-    pretrained_D15,
-    gpus16,
-    if_cache_gpu17,
-    if_save_every_weights18,
-    version19,
+        exp_dir1,
+        sr2,
+        if_f0_3,
+        spk_id5,
+        save_epoch10,
+        total_epoch11,
+        batch_size12,
+        if_save_latest13,
+        pretrained_G14,
+        pretrained_D15,
+        gpus16,
+        if_cache_gpu17,
+        if_save_every_weights18,
+        version19,
 ):
     # 生成filelist
     exp_dir = "%s/logs/%s" % (now_dir, exp_dir1)
@@ -491,10 +490,10 @@ def click_train(
         f0_dir = "%s/2a_f0" % (exp_dir)
         f0nsf_dir = "%s/2b-f0nsf" % (exp_dir)
         names = (
-            set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)])
-            & set([name.split(".")[0] for name in os.listdir(feature_dir)])
-            & set([name.split(".")[0] for name in os.listdir(f0_dir)])
-            & set([name.split(".")[0] for name in os.listdir(f0nsf_dir)])
+                set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)])
+                & set([name.split(".")[0] for name in os.listdir(feature_dir)])
+                & set([name.split(".")[0] for name in os.listdir(f0_dir)])
+                & set([name.split(".")[0] for name in os.listdir(f0nsf_dir)])
         )
     else:
         names = set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)]) & set(
@@ -569,42 +568,42 @@ def click_train(
             f.write("\n")
     if gpus16:
         cmd = (
-            '"%s" infer/modules/train/train.py -e "%s" -sr %s -f0 %s -bs %s -g %s -te %s -se %s %s %s -l %s -c %s -sw %s -v %s'
-            % (
-                config.python_cmd,
-                exp_dir1,
-                sr2,
-                1 if if_f0_3 else 0,
-                batch_size12,
-                gpus16,
-                total_epoch11,
-                save_epoch10,
-                "-pg %s" % pretrained_G14 if pretrained_G14 != "" else "",
-                "-pd %s" % pretrained_D15 if pretrained_D15 != "" else "",
-                1 if if_save_latest13 == i18n("是") else 0,
-                1 if if_cache_gpu17 == i18n("是") else 0,
-                1 if if_save_every_weights18 == i18n("是") else 0,
-                version19,
-            )
+                '"%s" infer/modules/train/train.py -e "%s" -sr %s -f0 %s -bs %s -g %s -te %s -se %s %s %s -l %s -c %s -sw %s -v %s'
+                % (
+                    config.python_cmd,
+                    exp_dir1,
+                    sr2,
+                    1 if if_f0_3 else 0,
+                    batch_size12,
+                    gpus16,
+                    total_epoch11,
+                    save_epoch10,
+                    "-pg %s" % pretrained_G14 if pretrained_G14 != "" else "",
+                    "-pd %s" % pretrained_D15 if pretrained_D15 != "" else "",
+                    1 if if_save_latest13 == i18n("是") else 0,
+                    1 if if_cache_gpu17 == i18n("是") else 0,
+                    1 if if_save_every_weights18 == i18n("是") else 0,
+                    version19,
+                )
         )
     else:
         cmd = (
-            '"%s" infer/modules/train/train.py -e "%s" -sr %s -f0 %s -bs %s -te %s -se %s %s %s -l %s -c %s -sw %s -v %s'
-            % (
-                config.python_cmd,
-                exp_dir1,
-                sr2,
-                1 if if_f0_3 else 0,
-                batch_size12,
-                total_epoch11,
-                save_epoch10,
-                "-pg %s" % pretrained_G14 if pretrained_G14 != "" else "",
-                "-pd %s" % pretrained_D15 if pretrained_D15 != "" else "",
-                1 if if_save_latest13 == i18n("是") else 0,
-                1 if if_cache_gpu17 == i18n("是") else 0,
-                1 if if_save_every_weights18 == i18n("是") else 0,
-                version19,
-            )
+                '"%s" infer/modules/train/train.py -e "%s" -sr %s -f0 %s -bs %s -te %s -se %s %s %s -l %s -c %s -sw %s -v %s'
+                % (
+                    config.python_cmd,
+                    exp_dir1,
+                    sr2,
+                    1 if if_f0_3 else 0,
+                    batch_size12,
+                    total_epoch11,
+                    save_epoch10,
+                    "-pg %s" % pretrained_G14 if pretrained_G14 != "" else "",
+                    "-pd %s" % pretrained_D15 if pretrained_D15 != "" else "",
+                    1 if if_save_latest13 == i18n("是") else 0,
+                    1 if if_cache_gpu17 == i18n("是") else 0,
+                    1 if if_save_every_weights18 == i18n("是") else 0,
+                    version19,
+                )
         )
     logger.info("Execute: " + cmd)
     p = Popen(cmd, shell=True, cwd=now_dir)
@@ -677,7 +676,7 @@ def train_index(exp_dir1, version19):
     yield "\n".join(infos)
     batch_size_add = 8192
     for i in range(0, big_npy.shape[0], batch_size_add):
-        index.add(big_npy[i : i + batch_size_add])
+        index.add(big_npy[i: i + batch_size_add])
     faiss.write_index(
         index,
         "%s/added_IVF%s_Flat_nprobe_%s_%s_%s.index"
@@ -713,24 +712,24 @@ def train_index(exp_dir1, version19):
 
 # but5.click(train1key, [exp_dir1, sr2, if_f0_3, trainset_dir4, spk_id5, gpus6, np7, f0method8, save_epoch10, total_epoch11, batch_size12, if_save_latest13, pretrained_G14, pretrained_D15, gpus16, if_cache_gpu17], info3)
 def train1key(
-    exp_dir1,
-    sr2,
-    if_f0_3,
-    trainset_dir4,
-    spk_id5,
-    np7,
-    f0method8,
-    save_epoch10,
-    total_epoch11,
-    batch_size12,
-    if_save_latest13,
-    pretrained_G14,
-    pretrained_D15,
-    gpus16,
-    if_cache_gpu17,
-    if_save_every_weights18,
-    version19,
-    gpus_rmvpe,
+        exp_dir1,
+        sr2,
+        if_f0_3,
+        trainset_dir4,
+        spk_id5,
+        np7,
+        f0method8,
+        save_epoch10,
+        total_epoch11,
+        batch_size12,
+        if_save_latest13,
+        pretrained_G14,
+        pretrained_D15,
+        gpus16,
+        if_cache_gpu17,
+        if_save_every_weights18,
+        version19,
+        gpus_rmvpe,
 ):
     infos = []
 
@@ -747,8 +746,8 @@ def train1key(
     [
         get_info_str(_)
         for _ in extract_f0_feature(
-            gpus16, np7, f0method8, if_f0_3, exp_dir1, version19, gpus_rmvpe
-        )
+        gpus16, np7, f0method8, if_f0_3, exp_dir1, version19, gpus_rmvpe
+    )
     ]
 
     # step3a:训练模型
@@ -784,7 +783,7 @@ def change_info_(ckpt_path):
         return {"__type__": "update"}, {"__type__": "update"}, {"__type__": "update"}
     try:
         with open(
-            ckpt_path.replace(os.path.basename(ckpt_path), "train.log"), "r"
+                ckpt_path.replace(os.path.basename(ckpt_path), "train.log"), "r"
         ) as f:
             info = eval(f.read().strip("\n").split("\n")[0].split("\t")[-1])
             sr, f0 = info["sample_rate"], info["if_f0"]
@@ -1175,7 +1174,7 @@ with gr.Blocks(title="RVC WebUI") as app:
                 )
             )
             with gr.Row():
-                exp_dir1 = gr.Textbox(label=i18n("输入实验名"), value="mi-test")
+                exp_dir1 = gr.Textbox(label=i18n("输入实验名"), value=f"mi-test-{uuid.uuid4()}")
                 sr2 = gr.Radio(
                     label=i18n("目标采样率"),
                     choices=["40k", "48k"],
