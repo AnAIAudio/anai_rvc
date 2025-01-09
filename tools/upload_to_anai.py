18ed25980fcd8f4a0de833f66f702669eac54bb4
hanbyulkang <kanghanstar@gmail.com>
2025-01-09 16:59:43 +0900

    포맷팅 수정 및 Black 도구 통합
    upload_to_anai.py의 코드 포맷을 개선했으며, requirements.txt에 Black을 추가했습니다. 또한, .idea 설정 파일에서 Black 자동 포맷팅 옵션을 활성화했습니다.

--- tools/upload_to_anai.py
+++ tools/upload_to_anai.py
@@ -7,7 +7,9 @@ def upload_to_anai(file):
     upload_path = ""
     for index, item in enumerate(file):
         file_extension = os.path.splitext(item)[1]
-        upload_path = os.path.join(os.getcwd(), 'temp_audio', f"{uuid.uuid4()}{file_extension}")
+        upload_path = os.path.join(
+            os.getcwd(), "temp_audio", f"{uuid.uuid4()}{file_extension}"
+        )
 
         if not os.path.exists(upload_path):
             os.makedirs(upload_path, exist_ok=True)
